18728b226c5dabe75bc3b1d13e7ea73e2def1055
BugHunter-X <askzenai@gmail.com>
2025-02-07 22:08:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,13 @@
+```php
+function foo(array $arr): void {
+    foreach ($arr as $key => $value) {
+        if ($value === 'bar') {
+            unset($arr[$key]);
+        }
+    }
+}
+
+$arr = ['foo', 'bar', 'baz'];
+foo($arr);
+print_r($arr); // Output: Array ( [0] => foo [2] => baz )
+```
